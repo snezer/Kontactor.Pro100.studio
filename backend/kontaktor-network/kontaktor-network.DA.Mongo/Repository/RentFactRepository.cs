a8f5d099be7ce5ab065f3dce995547bf1441537f
gribkovav <gribkovav@gmail.com>
2021-08-21 01:52:30 +0300

    Запрос аренды, аренда по арендаторам, аренда по помещениям

---
+++ backend/kontaktor-network/kontaktor-network.DA.Mongo/Repository/RentFactRepository.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using KONTRAKTOR.DA.Models;
+using MongoDB.Driver;
+
+namespace KONTAKTOR.DA.Mongo.Repository
+{
+    public class RentFactRepository: BaseMongoRepository<RentFact>
+    {
+        public RentFactRepository(MongoConnectionOptions settings, UserInformationRepository users) : base(settings)
+        {
+        }
+
+        public async Task<IEnumerable<RentFact>> GetRentsForTenantId(string id)
+        {
+
+            var result = (await _collection.FindAsync<RentFact>(x => x.TenantId == id)).ToEnumerable();
+            return result;
+        }
+
+        public async Task<IEnumerable<RentFact>> GetRentsForCompartment(string id)
+        {
+
+            var result = (await _collection.FindAsync<RentFact>(x => x.CompartmentId == id)).ToEnumerable();
+            return result;
+        }
+
+        public async Task<IEnumerable<RentFact>> GetRentsForCompartment(string id, DateTime from, DateTime to)
+        {
+
+            var result = (await _collection.FindAsync<RentFact>(x => x.CompartmentId == id && x.ContractStart>=from && x.ContractEnd<=to)).ToEnumerable();
+            return result;
+        }
+    }
+}
