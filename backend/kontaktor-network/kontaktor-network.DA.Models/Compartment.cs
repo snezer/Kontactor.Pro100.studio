3d414f8ee03d4b07e5970a728bffe71b3930a461
gribkovav <gribkovav@gmail.com>
2021-08-21 02:41:57 +0300

    MaxPeopleNumber

--- backend/kontaktor-network/kontaktor-network.DA.Models/Compartment.cs
+++ backend/kontaktor-network/kontaktor-network.DA.Models/Compartment.cs
@@ -5,17 +5,38 @@ using KONTAKTOR.DA.Models;
 
 namespace KONTAKTOR.DA.Models
 {
+    /// <summary>
+    /// Помещение
+    /// </summary>
     public class Compartment : BaseModel
     {
+        /// <summary>
+        /// Название помещения
+        /// </summary>
         public string Name { get; set; }
+        /// <summary>
+        /// Код помещения
+        /// </summary>
         public string ShortNameOrCode { get; set; }
+        /// <summary>
+        /// Арендуется ли в принципе или это служебное помещение/общего пользования
+        /// </summary>
         public bool IsForRent { get; set; }
+        /// <summary>
+        /// Разрешена долговременная аренда
+        /// </summary>
         public bool IsForLongTermRent { get; set; }
         /// <summary>
         /// Площадь
         /// </summary>
         public decimal Area { get; set; }
-
+        /// <summary>
+        /// Ссылка на карту
+        /// </summary>
         public string MapRoomId { get; set; }
+        /// <summary>
+        /// Максимальное количество размещаемых людей
+        /// </summary>
+        public int MaxPeopleNumber { get; set; }
     }
 }
