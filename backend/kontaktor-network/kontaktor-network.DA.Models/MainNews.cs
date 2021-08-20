49c01cdc4e066c9b609bd182dd4d96eee1c74373
gribkovav <gribkovav@gmail.com>
2021-08-21 02:04:58 +0300

    Скачивание блоба As is

---
+++ backend/kontaktor-network/kontaktor-network.DA.Models/MainNews.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using KONTAKTOR.DA.Models;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace kontaktor_network.DA.Models
+{
+    public class MainNews : BaseModel
+    {
+        public string Subject { get; set; }
+        public string Body { get; set; }
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string ContentId { get; set; }
+    }
+}
