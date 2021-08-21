6b1a16863a4dfd207b2a3ac942c8de80267750f7
FokinMU <FokinMU@refactorx.ru>
2021-08-21 07:32:34 +0700

    Events

--- AndroidClient/app/src/main/java/pro100/studio/contactor/MainActivity.java
+++ AndroidClient/app/src/main/java/pro100/studio/contactor/MainActivity.java
@@ -2,13 +2,33 @@ package pro100.studio.contactor;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btnLogin;
+    EditText edLogin;
+    EditText edPass;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnLogin = (Button) findViewById(R.id.btnLogin);
+        edLogin = (EditText) findViewById(R.id.edLogin);
+        edPass = (EditText) findViewById(R.id.edPassword);
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), EventActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
