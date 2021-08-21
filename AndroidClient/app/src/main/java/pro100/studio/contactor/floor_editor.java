d5876ab663aba34dd67d310c9ed385acc7f83276
FokinMU <FokinMU@refactorx.ru>
2021-08-21 17:43:04 +0700

    Floor view

---
+++ AndroidClient/app/src/main/java/pro100/studio/contactor/floor_editor.java
@@ -0,0 +1,57 @@
+package pro100.studio.contactor;
+
+import androidx.annotation.FloatRange;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Button;
+import android.widget.ImageView;
+
+public class floor_editor extends AppCompatActivity {
+
+    private WebView webView;
+    private Button btnDemo;
+    private ImageView btnBack;
+
+    private String FLOOR1 = "";
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_floor_editor);
+
+        webView = (WebView) findViewById(R.id.wv);
+        btnDemo = (Button) findViewById(R.id.demo);
+        btnDemo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), EventActivity.class);
+                startActivity(intent);
+            }
+        });
+        btnBack = (ImageView) findViewById(R.id.back);
+        btnBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), EventActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    private void LoadData(){
+        webView.loadData(FLOOR1, "text/html", "UTF-8");
+        webView.setBackgroundColor(Color.TRANSPARENT);
+        webView.setLayerType(WebView.LAYER_TYPE_SOFTWARE, null);
+        webView.getSettings().setBuiltInZoomControls(true);
+        webView.getSettings().setLoadWithOverviewMode(true);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.setWebViewClient(new WebViewClient());
+    }
+}
